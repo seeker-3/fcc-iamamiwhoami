357a4fe6c185b7b16d3dcfa13908b0d9cc9ac3a7
Glitch (fcc-iamamiwhoami-app) <none>
2018-04-14 09:17:13 +0000

    📉🐛 Checkpoint ./server.js:855406/371

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const os = require('os');
 const express = require('express');
 const app = express();
 
 function getData(str) {
-  var arr = [''];
+  var arr = [];
   var append = false;
   for (let c of str) {
-    if (append) arr[arr.length-1] += c;
     if (c === '(') {
-      append = true;
-    } else if (c === ')') {
       arr.push('');
+      append = true;
+    } else if (c === ')')
       append = false;
-    }
+    else if (append)
+      arr[arr.length-1] += c;
   }
+  return arr;
 }
 
 app.get("/", (req, res) => {
   res.json({
-    IP: req.ip.match(/\d.+/),
+    IP: req.ip.match(/\d.+/)[0],
     Lang: req.headers["accept-language"].split(',')[0],
-    OS: req.headers['user-agent'].match(/\(.+\)/),
+    OS: getData(req.headers['user-agent'])[0],
   });
   res.end();
 });
 
-
-const listener = app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${listener.address().port}`)
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log('listening on port' + port)
 });
