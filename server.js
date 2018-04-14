b92a1b865f28284c3335e4f7f3db9e9a5bebf9a8
Glitch (fcc-iamamiwhoami-app) <none>
2018-04-14 08:56:17 +0000

    😬⭐️ Checkpoint ./server.js:855406/200

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.json({
-    IP: req.socket.remoteAddress.match(/\d\S+/),
-    Lang: req.headers["accept-language"].match(/^(.+?),/)[1],
-    OS: os.platform() + ' ' + os.release(),
+    IP: req.iheaders['x-forwarded-for'].split(',')[0],
+    Lang: req.headers["accept-language"].split(',')[0],
+    OS: req.headers['user-agent'] //os.platform() + ' ' + os.release(),
   });
   res.end();
 });
